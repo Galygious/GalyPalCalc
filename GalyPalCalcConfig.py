3725c9fa0ddcd247dae025d68d86db0668c5d654
Galygious <61418882+Galygious@users.noreply.github.com>
2024-02-01 23:50:26 -0600

    Create GalyPalCalcConfig.py
    Creating Config File

---
+++ GalyPalCalcConfig.py
@@ -0,0 +1,23 @@
+# Define the maximum number of Pals
+max_pals = 13
+
+# Include pals with infusion?
+Infused = False
+
+# Define a blacklist of pal names
+blacklist = ['Frostallion Noct', 'Frostallion', 'Jetragon', 'Necromus', 'Paladius', 'Jormuntide Ignis']
+
+# Define minimum exprating requirements for each skill
+min_exprating_requirements = {
+    'Kindling': 16,
+    'Watering': 32,
+    'Planting': 32,
+    'Electricity': 16,
+    'Handiwork': 32,
+    'Gathering': 32,
+    'Lumbering': 16,
+    'Mining': 32,
+    'Medicine': 16,
+    'Cooling': 16,
+    'Transporting': 32
+}
